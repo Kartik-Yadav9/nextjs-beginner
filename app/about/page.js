a2e1f4f0384aab0b39d9c28bc21dfad5d082dcd1
kartik <kartikyadav0999@gmail.com>
2025-02-11 02:35:29 +0530

    begin

---
+++ app/about/page.js
@@ -0,0 +1,18 @@
+// "use client"
+import React from 'react'
+
+export const metadata = {
+  title: "Create kartik App",
+  description: "homezzzz",
+};
+
+function Aboutpage() {
+
+    console.log('hiiiii');
+    
+  return (
+    <div>Aboutpage</div>
+  )
+}
+
+export default Aboutpage
---
+++ app/page.js
@@ -0,0 +1,15 @@
+// "use client"
+// import { useState } from "react";
+
+export default function Home() {
+ 
+  // let[count, setCount]= useState(0)
+  // console.log('hi');
+  
+  return (
+    <div>
+        {/* count:{count}
+        <button onClick={()=>setCount(count+1)}>click</button> */}
+    </div>
+  );
+}
